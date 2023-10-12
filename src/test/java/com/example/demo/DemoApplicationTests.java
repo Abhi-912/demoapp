e0db869b3f23658346b5c8c44e3984f80951ace8
Abhishek pandey <56739045+Abhi-912@users.noreply.github.com>
2023-10-12 13:57:28 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,8 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		boolean a = true;
+		Assertions.assertEquals(true, a);
 	}
 
 }
